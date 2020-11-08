d20b3ba483c9f1c6e8e5b00dce5abda23abb6987
Maduri Vassaramo <maduri.vassaramo@gmail.com>
2020-11-08 09:10:21 +0000

    add test scenarios for checkout total method

---
+++ spec/checkout_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe Checkout do
+
+  describe '#scan' do
+
+  end
+
+  describe '#total' do
+    it 'is 0 if the basket is empty' do
+    end
+    
+    it 'applies basket discount if above £60' do
+    end
+
+    it 'applies discount for items with promotion' do
+    end
+
+    it 'applies both item and basket discount' do
+    end
+  end
+end
