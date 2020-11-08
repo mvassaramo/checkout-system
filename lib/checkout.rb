2eff5a5cfa647340775bc77d3b776adf3666adeb
Maduri Vassaramo <maduri.vassaramo@gmail.com>
2020-11-08 10:02:30 +0000

    wip: scan method - adds item to basket

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -2,9 +2,25 @@ require_relative 'item'
 require_relative 'promotion'
 
 class Checkout
+  
+  attr_reader :basket
+
   def initialize(promotional_rules)
     @basket = []
     create_promotions(promotional_rules)
+  end
+
+  def scan(item)
+    # check if valid
+
+    # check if basket has item
+    item_found = basket_has_item(item)
+
+    if item_found
+      item_found[:quantity] += 1
+    else
+      @basket << { code: item.code, price: item.price, quantity: 1 }
+    end
   end
 
   private 
@@ -12,5 +28,10 @@ class Checkout
   def create_promotions(promotional_rules)
     return if !promotional_rules
     promotional_rules.each { |promo| Promotion.new(promo) }
+  end
+
+  def basket_has_item(item)
+    false
+    # TODO: implement
   end
 end
--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,6 @@
 class Item
+  attr_reader :code, :name, :price
+
   def initialize(code:, name:, price:)
     @code = code
     @name = name
