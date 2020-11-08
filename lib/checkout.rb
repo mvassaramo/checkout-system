6d6882a04015dcaeb2f700d0c70660c66752d756
Maduri Vassaramo <maduri.vassaramo@gmail.com>
2020-11-08 09:55:58 +0000

    update checkout initialize method to create promotions

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,7 +1,16 @@
 require_relative 'item'
+require_relative 'promotion'
 
 class Checkout
   def initialize(promotional_rules)
+    @basket = []
+    create_promotions(promotional_rules)
+  end
 
+  private 
+
+  def create_promotions(promotional_rules)
+    return if !promotional_rules
+    promotional_rules.each { |promo| Promotion.new(promo) }
   end
 end
